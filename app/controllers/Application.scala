551e5bcc61e5cb64acdc1970eb9fdcb624bf80a3
Eric Coffin <eric.coffin@gmail.com>
2015-05-27 22:38:39 -0500

    first commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+    Thread.sleep(20)
+    Ok(
+      """
+        |{
+        |    "glossary": {
+        |        "title": "example glossary",
+        |		"GlossDiv": {
+        |            "title": "S",
+        |			"GlossList": {
+        |                "GlossEntry": {
+        |                    "ID": "SGML",
+        |					"SortAs": "SGML",
+        |					"GlossTerm": "Standard Generalized Markup Language",
+        |					"Acronym": "SGML",
+        |					"Abbrev": "ISO 8879:1986",
+        |					"GlossDef": {
+        |                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+        |						"GlossSeeAlso": ["GML", "XML"]
+        |                    },
+        |					"GlossSee": "markup"
+        |                }
+        |            }
+        |        }
+        |    }
+        |}
+      """.stripMargin).as("application/json")
+  }
+
+}
